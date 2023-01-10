74aaa40ab1882a61cf0745e944fd5596f04a27fe
t27133 <carlosnole@bcp.com.pe>
2023-01-09 21:48:26 -0500

    feat: app with hibernate and swagger

---
+++ product/src/main/java/org/digitalthinking/repositories/ProductRepository.java
@@ -0,0 +1,33 @@
+package org.digitalthinking.repositories;
+
+import org.digitalthinking.entites.Product;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
+public class ProductRepository {
+    @Inject
+    EntityManager em;
+
+    @Transactional
+    public void createdProduct(Product p){
+        em.persist(p);
+
+    }
+
+    @Transactional
+    public void deleteProduct(Product p){
+        em.remove(p);
+
+    }
+
+    @Transactional
+    public List<Product> listProduct(){
+        List<Product> products = em.createQuery("select p from Product p").getResultList();
+        return products;
+    }
+}
